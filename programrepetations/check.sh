55ec1241b9d60fbb0987f1279ddc8fba738b58ff
shridhar <beaprogrammer24@gmail.com>
2021-07-28 19:05:21 +0530

    first commit

---
+++ programrepetations/check.sh
@@ -0,0 +1,46 @@
+#!   /bin/bash  -x
+
+
+read -p "enter the number;" day
+
+
+
+
+if [ $day -eq 1 ]
+then
+    echo " monday "
+
+
+
+elif [ $day -eq 2 ]
+then
+    echo " tuesday "
+
+
+
+elif [ $day -eq 3 ]
+then
+    echo " wednsday "
+
+
+elif [ $day -eq 4 ]
+then
+    echo " thrusday "
+
+
+elif [ $day -eq 5 ]
+then
+    echo " friday "
+
+
+elif [ $day -eq 6 ]
+then
+    echo " saturday "
+
+
+elif [ $day -eq 7 ]
+then
+    echo " sunday "
+fi
+
+
